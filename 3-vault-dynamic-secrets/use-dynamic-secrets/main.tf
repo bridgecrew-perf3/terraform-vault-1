300ef79d71e1b86d5968d5500fe58710482f6843
Japneet Sahni <japneet_s.sahni@bell.ca>
2021-07-24 13:18:50 -0400

    Adding access and secret key to backend

--- 3-vault-dynamic-secrets/use-dynamic-secrets/main.tf
+++ 3-vault-dynamic-secrets/use-dynamic-secrets/main.tf
@@ -1,16 +1,6 @@
-data "terraform_remote_state" "admin" {
-  backend = "gcs"
-
-  config = {
-    bucket      = "terraform-state-japneet-arctiq"
-    prefix      = "3a-create-dynamic-secrets"
-    credentials = "serviceaccount-auth.json"
-  }
-}
-
 data "vault_aws_access_credentials" "creds" {
-  backend = data.terraform_remote_state.admin.outputs.backend
-  role    = data.terraform_remote_state.admin.outputs.role
+  backend = "aws"
+  role    = "terraform-role"
 }
 
 #This will generate new IAM user using vault dynamic secrets
